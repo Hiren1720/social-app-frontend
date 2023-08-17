7b64c82dea1d89a16b962b3848fe62f6e4ffe124
Hiren1722 <hirenbhuva2000@gmail.com>
2023-08-17 23:51:31 +0530

    fix origin issue

--- src/SSE/sse.js
+++ src/SSE/sse.js
@@ -6,5 +6,8 @@ export const STREAM_URL = `${url}/stream`;
 export const ssEvents = new EventSource(STREAM_URL);
 export const ably = new Ably.Realtime.Promise({
     authUrl: "https://ably.com/ably-auth/token/docs",
+    origin: [ 'https://social-v1-app.vercel.app', 'http://localhost:3000' ,'https://api.cloudinary.com/v1_1/'],
+    credentials: true,
+    optionSuccessStatus: 200
 });
 export const channel = ably.channels.get("social-app");
